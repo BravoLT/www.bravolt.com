37af9e85409555a64575b052a38b40a30fbb418e
Wise <ryan.wise@spectrumhealth.org>
2018-10-24 13:22:52 -0400

    fixed many issues

--- public/js/script.js
+++ public/js/script.js
@@ -132,82 +132,30 @@ $('.play-icon i').click(function() {
 	/*   Contact Form Validating
 	/* ========================================================================= */
 
+	$('#resumeUpload').on('change', function(e) {
+		var fileName = e.target.value.split('\\').pop();
+		if (fileName) {
+			$("#resumeUpload + label + .resume-name").html(fileName);
+		}
+	});
 
 	$('#contact-submit').click(function (e) {
-
-		//stop the form from being submitted
 		e.preventDefault();
 
-		/* declare the variables, var error is the variable that we use on the end
-		to determine if there was an error or not */
-		var error = false;
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var subject = $('#subject').val();
-		var message = $('#message').val();
+		var error = check_for_empty_inputs("#name, #email, #subject, #message");
 
-		/* in the next section we do the checking by using VARIABLE.length
-		where VARIABLE is the variable we are checking (like name, email),
-		length is a JavaScript function to get the number of characters.
-		And as you can see if the num of characters is 0 we set the error
-		variable to true and show the name_error div with the fadeIn effect.
-		if it's not 0 then we fadeOut the div( that's if the div is shown and
-		the error is fixed it fadesOut.
-
-		The only difference from these checks is the email checking, we have
-		email.indexOf('@') which checks if there is @ in the email input field.
-		This JavaScript function will return -1 if no occurrence have been found.*/
-		if (name.length == 0) {
-			var error = true;
-			$('#name').css("border-color", "#D8000C");
-		} else {
-			$('#name').css("border-color", "#666");
-		}
-		if (email.length == 0 || email.indexOf('@') == '-1') {
-			var error = true;
-			$('#email').css("border-color", "#D8000C");
-		} else {
-			$('#email').css("border-color", "#666");
-		}
-		if (subject.length == 0) {
-			var error = true;
-			$('#subject').css("border-color", "#D8000C");
-		} else {
-			$('#subject').css("border-color", "#666");
-		}
-		if (message.length == 0) {
-			var error = true;
-			$('#message').css("border-color", "#D8000C");
-		} else {
-			$('#message').css("border-color", "#666");
-		}
-
-		//now when the validation is done we check if the error variable is false (no errors)
 		if (error == false) {
-			//disable the submit button to avoid spamming
-			//and change the button text to Sending...
 			$('#contact-submit').attr({
 				'disabled': 'false',
 				'value': 'Sending...'
 			});
 
-			/* using the jquery's post(ajax) function and a lifesaver
-			function serialize() which gets all the data from the form
-			we submit it to send_email.php */
-			$.post("sendmail.php", $("#contact-form").serialize(), function (result) {
-				//and after the ajax request ends we check the text returned
-				if (result == 'sent') {
-					//if the mail is sent remove the submit paragraph
-					$('#cf-submit').remove();
-					//and show the mail success div with fadeIn
-					$('#mail-success').fadeIn(500);
-				} else {
-					//show the mail failed div
-					$('#mail-fail').fadeIn(500);
-					//re enable the submit button by removing attribute disabled and change the text back to Send The Message
-					$('#contact-submit').removeAttr('disabled').attr('value', 'Send The Message');
-				}
-			});
+			var subject = $("#name").val() + ": " + $("#subject").val();
+			var body = $("#message").val() + "\n\n" + "Email back at: " + $("#email").val();
+			aws_send_email(subject, body).then(() => $("#contact-submit").attr({
+				'value': 'Sent!',
+				'disabled': ''
+			}));
 		}
 	});
 
@@ -223,12 +171,9 @@ $('.play-icon i').click(function() {
 
 $(window).scroll(function() {
 var scroll = $(window).scrollTop();
- //console.log(scroll);
 if (scroll > 200) {
-    //console.log('a');
     $(".navigation").addClass("sticky-header");
 } else {
-    //console.log('a');
     $(".navigation").removeClass("sticky-header");
 }});
 
@@ -296,4 +241,42 @@ function initialize() {
 var map = $('#map');
 if(map.length != 0){
     google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+function aws_send_email (subject, body) {
+	return fetch('https://fwykqh9xdb.execute-api.us-west-2.amazonaws.com/production/bravolt-send-mail', {
+	  method: 'POST',
+	  headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({ subject, body })
+	})
+}
+
+function check_for_empty_inputs(queryString) {
+	var inputList = jQuery(queryString);
+	var error = false;
+
+	for (var i = 0; i < inputList.length; i++) {
+		if (inputList[i].value.length == 0) {
+			error = true;
+			$(inputList[i]).css("border-color", "#D8000C");
+		} else {
+			$(intputList[i]).css("border-color", "#666");
+		}
+	}
+
+	return error;
+}
+
+function file_to_base64_string (file) {
+	var fr = new FileReader();
+	fr.readAsDataURL(file);
+	fr.onload = function() { 
+		
+	}
+	fr.onerror = function(error) {
+		
+	}
 }
