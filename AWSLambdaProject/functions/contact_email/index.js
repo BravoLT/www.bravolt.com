4c98cc92a5cda23cccd3182d3bf29e0a4672dcdb
Ryan Wise <ryan.wise@wiseweb-design.com>
2018-10-25 00:37:35 -0400

    Added AWS lambda functions... created with apex

---
+++ AWSLambdaProject/functions/contact_email/index.js
@@ -0,0 +1,14 @@
+const sendEmail = require('./send_email')
+
+exports.handle = function(event, ctx, callback) {
+  console.log('processing event: %j', event)
+
+  if (event.subject && event.body) {
+    sendEmail(event.subject, event.body, event.resumeName || null, event.resume || null).then((result) => {
+      callback(null, result)
+    }, callback)
+
+  } else {
+    callback(new Error('Invalid payload: ' + JSON.stringify(event)))
+  }
+}
---
+++ AWSLambdaProject/functions/contact_email/send_email.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+const nodemailer = require('nodemailer');
+
+AWS.config.update({
+    region: process.env.SES_REGION
+});
+
+const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+
+module.exports = function sendEmail(subject, body, resumeName, resume) {
+    return new Promise((resolve, reject) => {
+        const params = {
+            from: process.env.FROM_EMAIL,
+            to: process.env.TO_EMAIL,
+            subject: subject,
+            html:  body
+        }
+
+        if (resumeName !== null && resume !== null) {
+            params.attachments = [
+                {
+                    filename: resumeName,
+                    resume: resume
+                }
+            ]
+        }
+        
+        console.log('e-mail params: ', params);
+        
+        var transporter = nodemailer.createTransport({
+            SES: ses
+        });
+
+        transporter.sendMail(params, (err, data) => {
+            if (err) {
+                console.error("Failed to send the email: ", err.stack || err);
+                reject(err);
+            } else {
+                console.log("e-mail sent: ", data)
+                resolve(data);
+            }
+        });
+    });
+}
